f683b379699db61b83489ca5f37ce633c1f12aaa
Cristopher Bohol <cris.bacus6@gmail.com>
2023-04-28 20:29:28 +0800

    default_link

--- src/views/Calculate/Calculate-Dashboard.vue
+++ src/views/Calculate/Calculate-Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container" v-if="$route.params.slug">
     <div class="tabs">
       <router-link
         v-for="loan_type in loan_types"
@@ -14,6 +14,19 @@
       <router-view></router-view>
     </div>
   </div>
+  <div class="container" v-else>
+    <div class="default-container" :class="{ centered: !$route.params.slug }">
+      <div v-if="!$route.params.slug" class="no-route">
+        <div class="image-section">
+          <img src="../../assets/images/Tax-Accountant.png" alt="Calculator" />
+          <h2>Calculate Loan</h2>
+          <button class="accent-btn" @click="openTabs">
+            Calculate and Apply Now
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -22,6 +35,11 @@
   import { mapState } from "vuex";
   export default {
     name: "CalculateView",
+    data() {
+      return {
+        showTabs: false,
+      };
+    },
     computed: {
       ...mapState({
         loan_types: (state) => state.loan_type.loan_type,
@@ -38,6 +56,10 @@
       console.log(this.loan_types);
     },
     methods: {
+      openTabs() {
+        this.showTabs = true;
+        this.goToloan_type(this.loan_types[0].slug);
+      },
       goToloan_type(slug) {
         this.$router.push({
           name: "calculate-view",
@@ -85,6 +107,38 @@
     color: #ffffff;
   }
 
+  .no-route {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+
+  .no-route h2 {
+    font-size: 2rem;
+    margin-bottom: 2rem;
+  }
+
+  .no-route img {
+    max-width: 50%;
+    margin-bottom: 2rem;
+  }
+
+  .accent-btn {
+    background-color: #4caf50;
+    color: #ffffff;
+    padding: 1rem 2rem;
+    border: none;
+    border-radius: 4px;
+    font-weight: bold;
+    cursor: pointer;
+  }
+
+  .accent-btn:hover {
+    background-color: #2e8b57;
+  }
+
   @media (max-width: 768px) {
     .tabs {
       flex-direction: column;
@@ -97,5 +151,26 @@
       height: 50px;
       margin-bottom: 5px;
     }
+  }
+
+  .image-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+  }
+
+  .image-container img {
+    max-width: 100%;
+    max-height: 100%;
+    object-fit: contain;
+  }
+
+  h2 {
+    margin: 20px 0;
+    font-size: 24px;
+    font-weight: bold;
+    text-align: center;
   }
 </style>
